8f64a6167222a2c6df67db4e11ad0c24964868da
vimal004 <2004.vimal@gmail.com>
2024-10-16 20:03:30 +0530

    Refactor stock routes to use findOneAndUpdate and findOneAndDelete instead of findByIdAndUpdate and findByIdAndDelete

--- frontend/backend/routes/stock.js
+++ frontend/backend/routes/stock.js
@@ -37,7 +37,7 @@ router.get("/:id", async (req, res) => {
 // Update Stock
 router.put("/:id", async (req, res) => {
   try {
-    const stock = await Stock.findOne(req.params.id, req.body, {
+    const stock = await Stock.findOneAndUpdate(req.params.id, req.body, {
       new: true,
     });
     if (!stock) return res.status(404).json({ message: "Stock not found" });
@@ -51,6 +51,7 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   try {
     const stock = await Stock.findOne(req.params.id);
+    stock.remove(); 
     if (!stock) return res.status(404).json({ message: "Stock not found" });
     res.json({ message: "Stock deleted successfully" });
   } catch (error) {
